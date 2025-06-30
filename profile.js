9795882b1bad404255b9eeccb94c9e01a885d94b
Ahmad2004 <113764852+ahemd2004@users.noreply.github.com>
2025-06-30 14:34:55 +0300

    Delete profile.js

--- profile.js
+++
@@ -1,7 +0,0 @@
-document.getElementById('logoutButton').addEventListener('click', () => {
-    fetch('/logout', { method: 'GET' })
-        .then(() => {
-            alert('Logged out successfully.');
-            window.location.href = '/login';
-        })
-});
